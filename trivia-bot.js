d29218df7a4e7333f8c3a6b5352cd80f6d229fdd
Kirill Petrozhitskii <kirill.petrozhitskii@gmail.com>
2026-07-31 00:38:36 +0200

    add trivia bot: variables, concatenation, reassignment

---
+++ trivia-bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm yout coding fun fact guide!");
+
+let botName = "Jarvis";
+let botLocation = "Israel";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " can run directly in the browser.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " was created in just 10 days.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " uses '===' to compare both value and data type.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
