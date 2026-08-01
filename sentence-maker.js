678f2ae61404426eabe7b081ab05110722adc97f
Kirill Petrozhitskii <kirill.petrozhitskii@gmail.com>
2026-08-01 12:22:33 +0200

    add sentence maker: variables, reassignment, string templates

---
+++ sentence-maker.js
@@ -0,0 +1,21 @@
+let adjective = "radioactive";
+let noun = "gecko";
+let verb = "hunting";
+let place = "cave near Goodsprings";
+let adjective2 = "glowing";
+let noun2 = "Brahmin steaks";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "fearless";
+noun = "courier";
+verb = "shot";
+place = "Mojave Wasteland";
+adjective2 = "heavily armed";
+noun2 = "Legion soldiers";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
